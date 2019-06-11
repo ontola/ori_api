c652b27c4b58e64c078cc5e0b3e6cb583811b099
Fletcher91 <thom@argu.co>
2019-06-11 09:14:58 +0200

    Streamline file headers

--- src/main/kotlin/io/ontola/ori/api/OriApi.kt
+++ src/main/kotlin/io/ontola/ori/api/OriApi.kt
@@ -1,6 +1,6 @@
 /*
  * ORI API
- * Copyright (C) 2019, Argu BV
+ * Copyright (C), Argu BV
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
